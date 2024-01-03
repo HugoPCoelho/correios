ae8edc6e140b48a39f06df22cf144a8b6890e2d6
HugoPCoelho <hugo.penha.coelho@gmail.com>
2024-01-03 18:23:01 -0300

    Commit inicial

---
+++ src/test/java/com/hugocoelho/correios/CorreiosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugocoelho.correios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorreiosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
